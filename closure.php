f213f60803ec9084da18213e6de446f358f3b9c3
Dane Odekirk <dizzzy@uw.edu>
2011-07-30 15:46:24 -0700

    Initial commit

---
+++ closure.php
@@ -0,0 +1,12 @@
+<?php
+
+    /*
+    Plugin Name: WP Closure  
+    Plugin URI: http://github.com/daneodekirk/wpclosure
+    Description: Uses Google Closure to minify and deliver Javascript files through a CDN.
+    Version: 0.1
+    Author: Dane Odekirk
+    */
+
+    
+?>
